e59c95b0257d03ddd326eee5a6aa3251532a7211
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-07-10 09:13:44 -0600

    Configurando el enrutador con react-router-dom en el docuemnto app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,33 @@ import Menu from "./components/Menu"
 import SubHeader from "./components/SubHeader"
 import Login from "./pages/Login"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useState } from "react"
 
 function App() {
+  
+ 
 
   return (
     <>
-      <Router> {/*BrowserRouter*/}
+{/*Peparando el enrutador */}
+    <Router>
         <div className="app-container">
-
-          <Header />
+{/*Pasando el prop de forma correcta para que al dar click en el boton el componente se renderice*/}
+          <Header/>
 
           <SubHeader />
           <Routes>
             <Route path="/" element={<Menu/>} />
-            <Route path="/Login" element={<Login/>} />
+            <Route path="/" element={<Login/>}/>
           </Routes>
+
+          <Menu/>
 
           <Footer />
 
         </div>
+    </Router>
 
-      </Router>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,23 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import { SlMenu } from "react-icons/sl";
 import { AiOutlineLogin } from "react-icons/ai";
 import { IoCartOutline } from "react-icons/io5";
 import Search from "./Search"; // componente para realizar busquedad
 
+/* Pasamos los props que se van a comunicar */
 const Header = () => {
+    
     return (
         <>
             <div className="header">
                 <div className="right-buttons">
-                    <button className="button"><SlMenu/></button>
+                    <button className="button" ><SlMenu/></button>
                 </div>
                 <div className="search-container">
                     <Search/>                    
                 </div>
                 <div className="left-buttons">
-                    <button className="button">Login
+                    <button  className="button">Login
                         <AiOutlineLogin />
                     </button>
                     <button className="button"><IoCartOutline/>
