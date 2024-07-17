6e6e05c94cd2ee178cd4bf2c4d30daac300c6059
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-07-17 00:20:09 -0600

    crando el formulario para el inicio de sesión haciendo el input de tipo passwod requerido

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,23 @@
 const Login = () => {
   return (
     <div className="login--container">Login
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis accusamus molestias voluptatum unde? Eius asperiores laborum omnis nulla ad. Praesentium iusto distinctio molestias velit? Ab error sed tempore quod fugiat?</p>
+      <div>
+        <form action="" className="fomr-login">
+          <label htmlFor="">Nombre: </label>
+          <input type="text" placeholder="Escribe tu nombre" /> <br />
+          <label htmlFor="">Apellidos: </label>
+          <input type="text" placeholder="Apellidos" /> <br />
+          <label htmlFor="">Fecha de nacimiento:</label>
+          <input type="date" name="" id="" /><br />
+          <label htmlFor="">Ingresa una contraseña: </label>
+          <input type="password" name="" id="" placeholder="crea una contraseña" /><br />
+          <label htmlFor="">Confirma tu contraseña: </label>
+          <input type="text" placeholder="Confirma tu contraseña" /> <br />
+          <button type="submit">enviar</button>
+
+
+        </form>
+      </div>
     </div>
   )
 }
