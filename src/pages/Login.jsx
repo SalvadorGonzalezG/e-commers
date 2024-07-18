ee8fbe9bd791b741612b946664441f0d92217cec
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-07-17 23:34:12 -0600

    Teminando el apartado de login y sus estilos

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,19 @@
+import { FiLogIn } from "react-icons/fi";
 
 const Login = () => {
   return (
-    <div className="login--container">Login
+    <div className="login--container">
       <div>
-        <form action="" className="fomr-login">
+        <form action="" className="form-login">
+          <h1>Login <FiLogIn/></h1>
           <label htmlFor="">Nombre: </label>
           <input type="text" placeholder="Escribe tu nombre" /> <br />
           <label htmlFor="">Apellidos: </label>
           <input type="text" placeholder="Apellidos" /> <br />
           <label htmlFor="">Fecha de nacimiento:</label>
-          <input type="date" name="" id="" /><br />
+          <input type="date" name="" id="" /> <br />
+          <label htmlFor="">Ingresa tu correo:</label>
+          <input type="email" name="" id="" placeholder="ejemplo@mail.com" required/> <br />
           <label htmlFor="">Ingresa una contraseña: </label>
           <input type="password" name="" id="" placeholder="crea una contraseña" /><br />
           <label htmlFor="">Confirma tu contraseña: </label>
