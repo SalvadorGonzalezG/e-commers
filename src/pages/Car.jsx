f7f6635159e40964177421a5d90f4e8edda3666d
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-07-20 23:15:03 -0600

    agregando items al carrito de compra agregando cada producto mediante context api => pasando toda la card y sus estilos de forma correcta

--- src/pages/Car.jsx
+++ src/pages/Car.jsx
@@ -1,9 +1,11 @@
 // Importamos el hook que hemos personalizado desde el archivo de contexto AppContext.jsx
 import { useAppContext } from "../context/AppContext"
 
+import { IoAdd } from "react-icons/io5";
+
 const Car = () => {
   // Usamos el hook personalizado para acceder al estado y la funcion de actualización de contexto
-  const {value, moreValue, items } = useAppContext();
+  const { value, moreValue, items } = useAppContext();
 
 
   return (
@@ -13,13 +15,46 @@ const Car = () => {
       {/*Boton para incrementar el numero de compras, llamando a funcioin moreValue al hacer click*/}
       <button onClick={moreValue}>Incrementar compras</button>
       <h2>Items en el carrito</h2>
-      <ul>{items.map((item, index)=>(
+      <ul>{items.map((item, index) => (
         <li key={index}>
           {item.name}
           {item.id}
           {item.pais}
-          <img src="{item.img}"  /></li>
+          <img src="{item.img}" />
+        </li>
       ))}</ul>
+    <div className="menu">
+      <div className="menu-container">
+        <div className="grid-container">
+          {items.map((item, index)=>
+        
+      <div className="grid-item" key={index}>
+        <div className='more-info'>
+          <button><IoAdd />
+          </button>
+        </div>
+        <div>
+                  <img className='img-central' src={item.img} alt="img" />
+                </div>
+                <div className='contenido'>
+                  <section>
+                    <h2> <i>ID:</i> {item.id}</h2>
+                    <i>name</i> {item.name} <br />
+                    <i>age:</i> {item.age} <br />
+                    <i>pais:</i> {item.pais} <br />
+                    <b>
+                      <i>@mail</i> {item.mail}
+                    </b>
+                  </section>
+                  
+                </div>
+                </div>
+                )}
+      </div>
+    </div>
+
+      </div>
+
     </div>
   )
 }
