e101ed43fa5611bdc2a447c999266908ff98fe3e
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-07-29 09:14:37 -0600

    Modificando el archivo Car.jsx quitando el iterador  .map que realizaba una copia de los elementos que ibas seleccionando en el boton agregar que se encuentra en Menu.jsx

--- src/pages/Car.jsx
+++ src/pages/Car.jsx
@@ -5,24 +5,12 @@ import { IoAdd } from "react-icons/io5";
 
 const Car = () => {
   // Usamos el hook personalizado para acceder al estado y la funcion de actualización de contexto
-  const {lessValue, value, moreValue, items } = useAppContext();
-
+  const {lessValue, moreValue, items } = useAppContext();
 
   return (
-    <div>
-      {/*Mostrar el número de compras en el carrito utilizando el valor del estado */}
-      <h1>Carrito de Comprastiene: {value} compras.</h1>
-      {/*Boton para incrementar el numero de compras, llamando a funcioin moreValue al hacer click*/}
-      <button onClick={moreValue}>Incrementar compras</button>
-      <h2>Items en el carrito</h2>
-      <ul>{items.map((item, index) => (
-        <li key={index}>
-          {item.name}
-          {item.id}
-          {item.pais}
-          <img src="{item.img}" />
-        </li>
-      ))}</ul>
+    <div className="buy-car">
+      
+      <h1 className="buyCarTitle">Productos en tu Carrito:</h1>
     <div className="menu">
       <div className="menu-container">
         <div className="grid-container">
